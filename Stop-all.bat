47a2b28a7492dcd6475ba3817e85c6309586e394
srinivashyd77 <srinivashyd77@gmail.com>
2018-11-11 16:06:14 +0530

    added zipkin and sleuth

---
+++ Stop-all.bat
@@ -0,0 +1,4 @@
+FOR /F "tokens=5 delims= " %%P IN ('netstat -a -n -o ^| findstr :8081') DO TaskKill.exe /PID %%P
+FOR /F "tokens=5 delims= " %%P IN ('netstat -a -n -o ^| findstr :8082') DO TaskKill.exe /PID %%P
+FOR /F "tokens=5 delims= " %%P IN ('netstat -a -n -o ^| findstr :8083') DO TaskKill.exe /PID %%P
+FOR /F "tokens=5 delims= " %%P IN ('netstat -a -n -o ^| findstr :8084') DO TaskKill.exe /PID %%P
